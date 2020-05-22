2d0a4c800a0c71ca10f4767901b4f5b0530cdad3
Matheus Menezes <mathsemilio@aspire-e1-572.168.0.108>
2020-05-22 14:34:27 -0300

    First Project Commit

---
+++ app/src/main/java/com/mathsemilio/hiraganalearner/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.mathsemilio.hiraganalearner.util
+
+// Constants for SharedPreferences
+const val GAME_SCORE_PREF = "GameScorePref"
+
+// Constants for the Logs tag
+const val TAG_MAIN_ACTIVITY = "MainActivity"
+const val TAG_SPLASH_SCREEN = "SpalshScreen"
+
+// Other
+const val DELAY_MILLIS = 2500L
