393a20131757623054c829084cc0fa1b020da727
mathsemilio <mathsemilio@gmail.com>
2020-09-12 21:25:07 -0300

    Removed Multi-Window support. App Build value changed to alpha-1.1

--- app/src/main/java/com/mathsemilio/hiraganalearner/ui/activity/SettingsActivity.kt
+++ app/src/main/java/com/mathsemilio/hiraganalearner/ui/activity/SettingsActivity.kt
@@ -34,7 +34,7 @@ class SettingsActivity : AppCompatActivity() {
 
     companion object {
         const val TRAINING_NOTIFICATION_TAG = "trainingNotification"
-        const val APP_BUILD_VERSION = "alpha-1.0"
+        const val APP_BUILD_VERSION = "alpha-1.1"
         const val NOTIFICATION_PREF_KEY = "notification"
         const val CLEAR_PERFECT_SCORES_PREF_KEY = "clearPerfectScores"
         const val APP_THEME_KEY = "appTheme"
