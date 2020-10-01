fe0dd4bfbd9d134454052e7932a6cb1c2859f3e9
mathsemilio <mathsemilio@gmail.com>
2020-10-01 19:08:11 -0300

    Removed and fixed some code commentary.

--- app/src/main/java/com/mathsemilio/hiraganalearner/data/model/HiraganaSymbol.kt
+++ app/src/main/java/com/mathsemilio/hiraganalearner/data/model/HiraganaSymbol.kt
@@ -1,6 +1,6 @@
 package com.mathsemilio.hiraganalearner.data.model
 
-// Data class for representing the hiragana symbols and their corresponding romanizations
+// Data class for representing the Hiragana symbols and their corresponding romanizations
 data class HiraganaSymbol(
     val symbol: String,
     val romanization: String
