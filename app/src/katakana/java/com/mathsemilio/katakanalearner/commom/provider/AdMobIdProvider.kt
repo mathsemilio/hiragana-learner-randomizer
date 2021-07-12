7e3b602ae8d43484282c87b439cc2fbffc0abbd2
Matheus Menezes <65728369+mathsemilio@users.noreply.github.com>
2021-07-11 21:36:30 -0300

    Deleted AdMobProvider.kt file

--- app/src/katakana/java/com/mathsemilio/katakanalearner/commom/provider/AdMobIdProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.mathsemilio.katakanalearner.commom.provider
-
-object AdMobIdProvider {
-    private const val APP_ID = "ca-app-pub-5242007423595790~9952474119"
-    private const val INTERSTITIAL_AD_ID = "ca-app-pub-5242007423595790/3550060758"
-    private const val BANNER_AD_ID = "ca-app-pub-5242007423595790/6129296095"
-
-    val interstitialAdId get() = INTERSTITIAL_AD_ID
-}
